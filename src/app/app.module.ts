74be8d607997c317eb7e59f3f2d3045b224e4c41
Akshay <akshayhiremath55@gmail.com>
2020-02-08 12:40:12 +0530

    Customer Application

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerModule } from './customer/app.customer.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+    
+  ],
+  imports: [
+        BrowserModule,
+    AppRoutingModule,
+    CustomerModule   
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
